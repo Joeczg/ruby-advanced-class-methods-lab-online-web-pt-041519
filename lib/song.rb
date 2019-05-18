85450cb287760d9c03a1d5a375fa0e3985a4761e
zhenggao cai <caizhengao1992@gmail.com>
2019-05-18 02:52:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,35 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+ 
   def self.all
     @@all
   end
-
+ 
   def save
     self.class.all << self
   end
-
+  
+  def self.create
+    song = self.new 
+    self.all << song.save
+    song
+  end
+  
+  def self.new_by_name(name)
+    song = self.new 
+    song.name = name 
+    song
+   end
+   
+   def self.create_by_name(name)
+     song = self.new 
+     song.name = name 
+     self.all << song.save
+     song 
+    end
+    
+    def self.find_by_name(name)
+      self.all.find {|song| song.name == name }
+    end
 end
